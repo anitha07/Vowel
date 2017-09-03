052c853f02358b4307f6e57ff37e605ba335a4aa
anitha07 <anithasakthivel20@gmail.com>
2017-09-03 08:32:07 -0700

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,30 @@
+# Vowel
+import java.io.*;
+import java.util.*;
+class Natural
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        switch(ch)
+        {
+            case 'a':
+                case 'A':
+                    case 'e':
+                        case 'E':
+                            case 'i':
+                                case 'I':
+                                    case 'o':
+                                        case 'O':
+                                            case 'u':
+                                                case 'U':
+                                                    System.out.println("Vowel");
+                                                    break;
+                                                    default:
+                                                    System.out.println("Consonant");
+                                                    break;
+        }
+        
+    }
+}
